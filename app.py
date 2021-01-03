f512bee1c072fe318f2b19d04179f164ee5512f5
freebomd <76235334+freebomd@users.noreply.github.com>
2021-01-03 03:37:41 -0800

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,10 @@ def signup():
 def reset():
     return render_template("reset.html")
 
+@app.errorhandler(404)
+def notfound(e):
+    return render_template("home.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port= 5000)
